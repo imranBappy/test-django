2e5b2e81c96f327282a67225a96c4252c685f34c
imranBappy <imranbappy.official.com>
2025-02-02 23:18:13 +0600

    update

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ pip install -r requirements.txt
 echo "Applying migrations..."
 python3 manage.py migrate
 
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
+# echo "Collecting static files..."
+# python3 manage.py collectstatic --noinput
 
 echo "Restarting Gunicorn..."
 pkill gunicorn
--- members/views.py
+++ members/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world! test")
+    return HttpResponse("Hello world! Imran")
